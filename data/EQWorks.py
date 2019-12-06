28e5a1d8172edf139a1f02749c493ebd86b5732d
Tuyen Do <Tuyen.Do@coyotes.usd.edu>
2019-12-06 14:43:23 -0600

    Submit data engineer part

--- data/EQWorks.py
+++ data/EQWorks.py
@@ -16,17 +16,16 @@ def getPipeline(goal, excep, rel):
     executed = []
     dependencies = [goal]
     while len(dependencies) != 0:
+        # Remove all the dependencies of start node and executed node
         while (dependencies[0] in excep) or (dependencies[0] in executed):
             dependencies.pop(0)
             if len(dependencies) == 0:
                 break
         if len(dependencies) == 0:
             continue
-        for i,e in enumerate(rel):
-            if e[0] == dependencies[0]:
-                dependencies = dependencies + e[1]
-                rel.pop(i)
-                break
+        dep = rel.lookup(dependencies[0])
+        if len(dep) != 0:
+            dependencies = dependencies + dep[0]
         executed.append(dependencies.pop(0))
     return executed
 
@@ -34,12 +33,13 @@ def main():
     spark = SparkSession.builder.master('spark://master:7077').appName("EQWork").getOrCreate()
     POIdf = spark.read.format("csv").option("header", "true").load("/tmp/data/POIList.csv")
     sampleDf = spark.read.format("csv").option("header", "true").load("/tmp/data/DataSample.csv")
-    # Filtered the identical geopoin and time
+    # 1) Filtered the identical geopoin and time
     cleanSample = sampleDf.dropDuplicates([' TimeSt','Latitude','Longitude'])
     poiDf = POIdf.drop_duplicates([' Latitude','Longitude'])
     poiList = poiDf.rdd.collect()
-    # Assign label to geopoint by the nearest POI
+    # 2) Assign label to geopoint by the nearest POI
     labelSample = cleanSample.rdd.map(lambda r: calDist(r,poiList)).toDF()
+    # 3) 1.Calculate the average and standar deviation
     # Calculate the average
     labelSample.groupBy('label').agg({'distance':'avg'})\
         .coalesce(1).write.format("csv").option("header", "true")\
@@ -48,15 +48,15 @@ def main():
     labelSample.groupBy('label').agg({'distance':'stddev'})\
         .coalesce(1).write.format("csv").option("header", "true")\
         .mode("append").save("/tmp/data/StandardDeviation")
+    # 3) 2.Calculate the radius and density of each POI
     radius = labelSample.groupBy('label').agg({'distance':'max'}).orderBy('label').collect()
     total = labelSample.groupBy('label').count().orderBy('label').collect()
-    dense = [(total[x]['label'],total[x]['count']/(math.pi * radius[x]['max(distance)']**2))\
-    for x in range(3)]
+    dense = [(total[x]['label'],total[x]['count']/(math.pi * radius[x]['max(distance)']**2)) for x in range(3)]
     # 4) Calculate the pipeline
-    # Create relations list
+    # Create relations list which reverse the order (dst,src)
     relations = spark.read.text('/tmp/data/relations.txt').rdd\
     .map(lambda l : (l[0].rstrip().split('->')[1],[l[0].split('->')[0]]))\
-    .reduceByKey(lambda x,y : x + y).collect()
+    .reduceByKey(lambda x,y : x + y)
 
     with open('/tmp/data/question.txt') as f:
         content = f.readlines()
@@ -65,13 +65,18 @@ def main():
     # Generate the pipeline for start and goal point
     depenStart = getPipeline(start,[''],relations)
     depenGoal = getPipeline(goal,depenStart,relations)
+    # reverse the order of list to get the right executed order
+    depenGoal.reverse()
     # Write output to file
     cleanSample.coalesce(1).write.format("csv").option("header", "true").mode("append").save("/tmp/data/FilteredData")
     labelSample.coalesce(1).write.format("csv").option("header", "true").mode("append").save("/tmp/data/LabeledData")
+    with open('/tmp/data/radius.txt','w') as f:
+        for poi in radius:
+            f.write(poi[0] + ": " + str(poi[1])+"\n")
     with open('/tmp/data/dense.txt','w') as f:
         for poi in dense:
-            f.write(poi[0] + ": " + str(poi[1]))
-    with open('/tmp/data/dense.txt','w') as f:
+            f.write(poi[0] + ": " + str(poi[1])+"\n")
+    with open('/tmp/data/pipeline.txt','w') as f:
         f.write(str(depenGoal))
 
 if __name__ == '__main__':
